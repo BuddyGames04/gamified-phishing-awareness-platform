7d6c2abec6c548388ba37efdf61ededeee1fa58b
Luke <cochranel@outlook.com>
2025-10-06 19:24:20 +0100

    Test connectivity of django backend API with the frontend

--- src/backend/core/settings.py
+++ src/backend/core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "corsheaders",
+    "api",
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
@@ -65,6 +70,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React dev server
 ]
 
 WSGI_APPLICATION = "core.wsgi.application"
--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/hello/")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+      .catch((err) => console.error("Error fetching:", err));
+  }, []);
+
+  return <h1>{message || "Loading..."}</h1>;
 }
 
 export default App;
+
+
