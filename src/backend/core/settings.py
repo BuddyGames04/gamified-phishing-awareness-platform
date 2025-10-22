5ff63aac19c7a1d65950c9d160c445c3645c7160
Luke <cochranel@outlook.com>
2025-10-22 13:08:26 +0100

    Fix flake linter issues

--- src/backend/core/settings.py
+++ src/backend/core/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "phishing_game",
+        "USER": "luke",
+        "PASSWORD": "yourpassword",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
